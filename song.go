6d573e19cd69e02b61e79146d376596149ac9979
Tom <josephdotson@belkonar.com>
2024-09-28 12:20:20 -0400

    playing notes

--- song.go
+++ song.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/whynotavailable/musak/engine"
 	"github.com/whynotavailable/musak/models"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv"
 	"gitlab.com/gomidi/midi/v2/smf"
+	"os"
 
 	"github.com/whynotavailable/musak/notes"
 	"gitlab.com/gomidi/midi/v2"
@@ -28,13 +30,45 @@ func main() {
 
 	midiData := engine.Compile(midiFile)
 
+	loopPlay(midiData)
+}
+
+func loopPlay(file []byte) {
+	sig := make(chan int, 1)
+	finish := make(chan int, 1)
+
+	go func() {
+		for {
+			select {
+			case <-sig:
+				finish <- 1
+				return
+			default:
+			}
+
+			playSong(file)
+		}
+	}()
+	_, _ = bufio.NewReader(os.Stdin).ReadBytes('\n')
+	sig <- 1
+	<-finish
+	reset()
+}
+
+func reset() {
+	out, _ := midi.FindOutPort("IAC Driver Bus 1")
+	send, _ := midi.SendTo(out)
+	_ = send(midi.Reset())
+}
+
+func playSong(file []byte) {
 	out, err := midi.FindOutPort("IAC Driver Bus 1")
 	if err != nil {
 		fmt.Printf("can't find qsynth")
 		return
 	}
 
-	rd := bytes.NewReader(midiData)
+	rd := bytes.NewReader(file)
 
 	// read and play it
 	fullTrack := smf.ReadTracksFrom(rd)
@@ -42,4 +76,5 @@ func main() {
 	fullTrack.Play(out)
 
 	fmt.Println(notes.A4)
+
 }
